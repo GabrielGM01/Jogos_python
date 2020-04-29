9b4e0efa0e8ab376a5d4ba7c2048053f9a357929
Gabriel Gusmão Moreno <59850618+GabrielGM01@users.noreply.github.com>
2020-04-29 18:55:41 -0300

    Adicionando_o_JogodaForca

---
+++ jogo_da_forca.py
@@ -0,0 +1,40 @@
+
+def jogar():
+    print("-------------------------------")
+    print("Bem vindo ao jogo da forca")
+    print("-------------------------------")
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    erros = 6
+    while(erros):
+        print(letras_acertadas)
+        tentativa = input("Digite uma Letra:")
+        tentativa = tentativa.strip().upper()
+
+        if(tentativa in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(tentativa == letra):
+                    letras_acertadas[index] = letra
+                index -= 1
+            acertou = "_" in letras_acertadas
+            if(acertou == False):
+                print(letras_acertadas)
+                print("A palavra secreta é {}".format(palavra_secreta))
+                print("Voce venceu")
+                break
+        else:
+            erros -= 1
+            if(erros == 0):
+                print("A palavra secreta é {}".format(palavra_secreta))
+                print("Voce perdeu")
+                break
+            print("Voce tem {} tentativas".format(erros))
+       
+
+
+
+if (__name__== "__main__"):
+
+    jogar() 
